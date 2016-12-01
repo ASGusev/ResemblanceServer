694fc14cec70e0550c8da62c4af7241dac1564e8
MZuev <Mzuev97@gmail.com>
2016-12-01 17:33:46 +0300

    server

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -5,6 +5,8 @@ import java.net.Socket;
 import java.util.HashMap;
 
 public class Server {
+    private final static int maxCntToReconnect = 10;
+    private final static int sleepTime = 1000;
     private static HashMap<String, ClientThread> clientTreadFromName = new HashMap<>();
     private static HashMap<Socket, String> clientNameFromSocket = new HashMap<>();
 
@@ -60,52 +62,124 @@ public class Server {
     }
 
     private static class ClientThread extends Thread {
-        Socket socket;
-        DataInputStream in = null;
-        DataOutputStream out = null;
-        Integer cnt = 0;
+        private Socket socket;
+        private DataInputStream in = null;
+        private DataOutputStream out = null;
+        private Boolean connectionError = false;
+        private String clientName = null;
 
         ClientThread(Socket socket) {
             this.socket = socket;
-            try {
-                in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-                out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-            }
-            catch (IOException e) {
-                System.out.println("Bad socket: " + socket);
-                this.stop();
-            }
-
         }
 
         public Socket getSocket() {
             return socket;
         }
         public String getCleintName() {
-            return Server.getName(socket);
+            return clientName;
+        }
+        public boolean isConnected() {
+            return !connectionError;
         }
 
         @Override
         public void run() {
-            while(true) {
+
+            //initThread
+
+            for (int i = 0; i < maxCntToReconnect; ++i) {
                 try {
-                    String textNewMessage = in.readUTF();
-                    Message newMessage = new Message(textNewMessage, socket);
-                    newMessage.applyMessage();
-                }
-                catch (IOException e) {
+                    if (in == null) {
+                        in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+                    }
+                    if (out == null) {
+                        out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+                    }
+                } catch (IOException e) {
                     e.printStackTrace();
+                    try {
+                        sleep(sleepTime);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                    continue;
                 }
+                break;
+            }
+            if (in == null || out == null) {
+                connectionError = true;
+                return;
+            }
+
+            //-----
+
+            while(true) {
+                String textNewMessage = null;
+                for (int i = 0; i < maxCntToReconnect; ++i) {
+                    try {
+                        textNewMessage = in.readUTF();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        try {
+                            sleep(sleepTime);
+                        } catch (InterruptedException e1) {
+                            e1.printStackTrace();
+                        }
+                        continue;
+                    }
+                    break;
+                }
+                if (textNewMessage == null) {
+                    synchronized (connectionError) {
+                        connectionError = true;
+                    }
+                    return;
+                }
+                System.out.println(textNewMessage);
+                Message newMessage = new Message(textNewMessage, socket);
+                newMessage.applyMessage();
             }
         }
-        synchronized private void sendMessage(String message) {
+
+        synchronized private boolean tryToSendMessage(String message) {
             try {
                 out.writeUTF(message);
                 out.flush();
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
+                return false;
             }
+            return true;
+        }
+
+        private void sendMessage(final String message) {
+            synchronized (connectionError) {
+                if (connectionError) {
+                    return;
+                }
+            }
+            Thread curThread =  new Thread() {
+                @Override
+                public void run() {
+                    boolean messageIsSent = false;
+                    for (int i = 0; i < maxCntToReconnect && !messageIsSent; i++) {
+                        messageIsSent = tryToSendMessage(message);
+                        if (!messageIsSent) {
+                            try {
+                                sleep(sleepTime);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                    if (!messageIsSent) {
+                        synchronized (connectionError) {
+                            connectionError = true;
+                        }
+                    }
+                }
+            };
+            curThread.start();
         }
     }
     public static Message TEST_MESSAGE = new Message("test test");
